5b245a2153fe6cd21341347ac6243b3722138997
Piseth Peou <ppeou@msn.com>
2018-10-04 23:35:21 -0500

    initial code upload

--- src/test/java/me/home/app/selenium/SeleniumApplicationTests.java
+++ src/test/java/me/home/app/selenium/SeleniumApplicationTests.java
@@ -1,9 +1,5 @@
 package me.home.app.selenium;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,25 +8,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Suite.SuiteClasses({ShadowDOMTest.class})
 @SpringBootTest
 public class SeleniumApplicationTests {
-	/*@BeforeClass
-	public static void testBeforeSuite() {
-		System.out.println("Before Class()");
-	}
 
-	@AfterClass
-	public static void testAfterSuite() {
-		System.out.println("After Class()");
-		System.out.println("dispose()");
-		//driver.quit();
-	}
-
-	@Before
-	public void setUp() {
-		System.out.println("setUp()");
-	}
-
-	@After
-	public void tearDown() {
-		System.out.println("tearDown()");
-	}*/
 }
--- src/test/java/me/home/app/selenium/WebDriverConfig.java
+++ src/test/java/me/home/app/selenium/WebDriverConfig.java
@@ -1,15 +1,7 @@
 package me.home.app.selenium;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
